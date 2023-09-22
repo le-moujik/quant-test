63d737bc33362f9a6e0d7c16ee3a82557f349dca
le-moujik <62592892+le-moujik@users.noreply.github.com>
2023-09-22 04:36:18 +0300

    Update predict.py

--- ml_task/predict.py
+++ ml_task/predict.py
@@ -8,13 +8,11 @@ OUTPUT_PATH = "ml_task/predictions.csv"
 def main():
     try:
         df = pd.read_csv("ml_task/hidden_test.csv")
-    except:
-        FileNotFoundError as e:
+    except FileNotFoundError as e:
         print(e); exit()
     try:
         pipeline = joblib.load("ml_task/model.joblib")
-    except:
-        FileNotFoundError as e:
+    except FileNotFoundError as e:
         print(e); exit()
     predictions = pipeline.predict(df[['6', '7']].values)
     # predictions = df['7'] + df['6']**2
