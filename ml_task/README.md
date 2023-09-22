081bac90e5db1405774a6bf51776b562aac1962e
le-moujik <62592892+le-moujik@users.noreply.github.com>
2023-09-22 04:13:16 +0300

    Add files via upload

---
+++ ml_task/README.md
@@ -0,0 +1,8 @@
+## To run scripts:
+1. Clone repository quant
+2. Create virtual env in the quant directory;
+4. Install requirements from the ml_task/requirement.txt;
+5. Add train.csv and hidden_test.csv files to the ml_task directory;
+6. Run script train.py;
+7. Run script predict.py;
+8. Check the results from the file predictions.csv.
---
+++ ml_task/train.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import joblib
+
+
+MODEL_PATH = "ml_task/model.joblib"
+
+
+def main():
+    try:
+        df = pd.read_csv("ml_task/train.csv")
+    except:
+        FileNotFoundError as e:
+        print(e); exit()
+    pipeline = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+    pipeline.fit(df[['6', '7']].values, df['target'].values)
+    joblib.dump(pipeline, MODEL_PATH)
+    print(f"model saved to {MODEL_PATH}")
+    
+    
+main()
